8930d56afeec90b3351ba66a746848382356d730
Philipp Spiegel <spiegelp@gmx.at>
2019-10-17 20:52:12 +0200

    added Month and extension methods to the time API

---
+++ NuniToolbox.Test/Time/MonthExtensionsTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+using Xunit;
+
+using NuniToolbox.Time;
+
+namespace NuniToolbox.Test.Time
+{
+    public class MonthExtensionsTest
+    {
+        [Theory]
+        [InlineData(Month.January, 31)]
+        [InlineData(Month.January, 31, true)]
+        [InlineData(Month.February, 28)]
+        [InlineData(Month.February, 29, true)]
+        [InlineData(Month.March, 31)]
+        [InlineData(Month.March, 31, true)]
+        [InlineData(Month.April, 30)]
+        [InlineData(Month.April, 30, true)]
+        [InlineData(Month.May, 31)]
+        [InlineData(Month.May, 31, true)]
+        [InlineData(Month.June, 30)]
+        [InlineData(Month.June, 30, true)]
+        [InlineData(Month.July, 31)]
+        [InlineData(Month.July, 31, true)]
+        [InlineData(Month.August, 31)]
+        [InlineData(Month.August, 31, true)]
+        [InlineData(Month.September, 30)]
+        [InlineData(Month.September, 30, true)]
+        [InlineData(Month.October, 31)]
+        [InlineData(Month.October, 31, true)]
+        [InlineData(Month.November, 30)]
+        [InlineData(Month.November, 30, true)]
+        [InlineData(Month.December, 31)]
+        [InlineData(Month.December, 31, true)]
+        public void Test_Days_Ok(Month month, int expectedDays, bool isLeapYear = false)
+        {
+            Assert.Equal(expectedDays, month.Days(isLeapYear));
+        }
+
+        [Theory]
+        [InlineData(Month.January, 1)]
+        [InlineData(Month.February, 2)]
+        [InlineData(Month.March, 3)]
+        [InlineData(Month.April, 4)]
+        [InlineData(Month.May, 5)]
+        [InlineData(Month.June, 6)]
+        [InlineData(Month.July, 7)]
+        [InlineData(Month.August, 8)]
+        [InlineData(Month.September, 9)]
+        [InlineData(Month.October, 10)]
+        [InlineData(Month.November, 11)]
+        [InlineData(Month.December, 12)]
+        public void Test_Number_Ok(Month month, int expectedNumber)
+        {
+            Assert.Equal(expectedNumber, month.Number());
+        }
+
+        [Fact]
+        public void Test_Name_Ok()
+        {
+            Month month = Month.March;
+            CultureInfo enCultureInfo = CultureInfo.CreateSpecificCulture("en");
+            CultureInfo deCultureInfo = CultureInfo.CreateSpecificCulture("de");
+
+            Assert.Equal("March", month.Name(enCultureInfo));
+            Assert.Equal("März", month.Name(deCultureInfo));
+        }
+
+        [Fact]
+        public void Test_ShortName_Ok()
+        {
+            Month month = Month.March;
+            CultureInfo enCultureInfo = CultureInfo.CreateSpecificCulture("en");
+            CultureInfo deCultureInfo = CultureInfo.CreateSpecificCulture("de");
+
+            Assert.Equal("Mar", month.ShortName(enCultureInfo));
+            Assert.Equal("Mrz", month.ShortName(deCultureInfo));
+        }
+    }
+}
---
+++ NuniToolbox/Time/MonthExtensions.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace NuniToolbox.Time
+{
+    public static class MonthExtensions
+    {
+        /// <summary>
+        /// Returns the number of days for the month.
+        /// </summary>
+        /// <param name="month"></param>
+        /// <param name="isLeapYear">Consider a leap year for February</param>
+        /// <returns></returns>
+        public static int Days(this Month month, bool isLeapYear = false)
+        {
+            switch (month)
+            {
+                case Month.January:
+                case Month.March:
+                case Month.May:
+                case Month.July:
+                case Month.August:
+                case Month.October:
+                case Month.December:
+                    return 31;
+
+                case Month.April:
+                case Month.June:
+                case Month.September:
+                case Month.November:
+                    return 30;
+
+                case Month.February:
+                    return isLeapYear ? 29 : 28;
+
+                default:
+                    throw new ArgumentException("invalid month argument", nameof(month));
+            }
+        }
+
+        /// <summary>
+        /// The number of the month starting by 1 for January.
+        /// </summary>
+        /// <param name="month"></param>
+        /// <returns></returns>
+        public static int Number(this Month month)
+        {
+            return (int)month;
+        }
+
+        /// <summary>
+        /// The full name of the month in the current culture.
+        /// </summary>
+        /// <param name="month"></param>
+        /// <returns></returns>
+        public static string Name(this Month month)
+        {
+            return month.Name(CultureInfo.CurrentCulture);
+        }
+
+        /// <summary>
+        /// The full name of the month in the specified culture.
+        /// </summary>
+        /// <param name="month"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public static string Name(this Month month, CultureInfo culture)
+        {
+            return FormatMonth(month, "MMMM", culture);
+        }
+
+        /// <summary>
+        /// The short name of the month in the current culture.
+        /// </summary>
+        /// <param name="month"></param>
+        /// <returns></returns>
+        public static string ShortName(this Month month)
+        {
+            return ShortName(month, CultureInfo.CurrentCulture);
+        }
+
+        /// <summary>
+        /// The short name of the month in the specified culture.
+        /// </summary>
+        /// <param name="month"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public static string ShortName(this Month month, CultureInfo culture)
+        {
+            return FormatMonth(month, "MMM", culture);
+        }
+
+        private static string FormatMonth(this Month month, string format, CultureInfo culture)
+        {
+            // use an arbitrary DateTime in the specified month and format it only with the full name of the month
+            DateTime dateTime = new DateTime(2019, month.Number(), 1);
+
+            return dateTime.ToString(format, culture);
+        }
+    }
+}
