0c1208a7773092239c4496f489337100e8aac967
Philipp Spiegel <spiegelp@gmx.at>
2019-10-20 13:54:42 +0200

    added LocalTime to the time API

---
+++ NuniToolbox.Test/Time/LocalTimeTest.cs
@@ -0,0 +1,425 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+using Xunit;
+
+using NuniToolbox.Time;
+
+namespace NuniToolbox.Test.Time
+{
+    public class LocalTimeTest
+    {
+        [Fact]
+        public void Test_CreateLocalTime_Ok()
+        {
+            LocalTime localTime = new LocalTime(2, 4);
+
+            Assert.Equal(2, localTime.Hours);
+            Assert.Equal(4, localTime.Minutes);
+            Assert.Equal(0, localTime.Seconds);
+            Assert.Equal(0, localTime.Milliseconds);
+
+            localTime = new LocalTime(2, 4, 8);
+
+            Assert.Equal(2, localTime.Hours);
+            Assert.Equal(4, localTime.Minutes);
+            Assert.Equal(8, localTime.Seconds);
+            Assert.Equal(0, localTime.Milliseconds);
+
+            localTime = new LocalTime(2, 4, 8, 16);
+
+            Assert.Equal(2, localTime.Hours);
+            Assert.Equal(4, localTime.Minutes);
+            Assert.Equal(8, localTime.Seconds);
+            Assert.Equal(16, localTime.Milliseconds);
+
+            localTime = new LocalTime(new DateTime(2019, 10, 17, 21, 15, 32, 512));
+
+            Assert.Equal(21, localTime.Hours);
+            Assert.Equal(15, localTime.Minutes);
+            Assert.Equal(32, localTime.Seconds);
+            Assert.Equal(512, localTime.Milliseconds);
+        }
+
+        [Fact]
+        public void Test_CreateLocalTime_ArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => new LocalTime(24, 0));
+            Assert.Throws<ArgumentException>(() => new LocalTime(-1, 0));
+            Assert.Throws<ArgumentException>(() => new LocalTime(0, 60));
+            Assert.Throws<ArgumentException>(() => new LocalTime(0, -1));
+            Assert.Throws<ArgumentException>(() => new LocalTime(0, 0, 60));
+            Assert.Throws<ArgumentException>(() => new LocalTime(0, 0, -1));
+            Assert.Throws<ArgumentException>(() => new LocalTime(0, 0, 0, 1000));
+            Assert.Throws<ArgumentException>(() => new LocalTime(0, 0, 0, -1));
+        }
+
+        [Fact]
+        public void Test_ToDateTime_Ok()
+        {
+            LocalTime localTime = new LocalTime(2, 4, 8, 16);
+            DateTime dateTime = localTime.ToDateTime();
+
+            Assert.Equal(localTime.Hours, dateTime.Hour);
+            Assert.Equal(localTime.Minutes, dateTime.Minute);
+            Assert.Equal(localTime.Seconds, dateTime.Second);
+            Assert.Equal(localTime.Milliseconds, dateTime.Millisecond);
+        }
+
+        [Fact]
+        public void Test_WithHours_Ok()
+        {
+            LocalTime localTime = new LocalTime(2, 4).WithHours(4);
+            LocalTime localTimeResult = new LocalTime(4, 4);
+
+            Assert.Equal(localTimeResult, localTime);
+        }
+
+        [Fact]
+        public void Test_WithHours_ArgumentException()
+        {
+            LocalTime localTime = new LocalTime(2, 4);
+
+            Assert.Throws<ArgumentException>(() => localTime.WithHours(64));
+            Assert.Throws<ArgumentException>(() => localTime.WithHours(-2));
+        }
+
+        [Fact]
+        public void Test_WithMinutes_Ok()
+        {
+            LocalTime localTime = new LocalTime(2, 4).WithMinutes(8);
+            LocalTime localTimeResult = new LocalTime(2, 8);
+
+            Assert.Equal(localTimeResult, localTime);
+        }
+
+        [Fact]
+        public void Test_WithMinutes_ArgumentException()
+        {
+            LocalTime localTime = new LocalTime(2, 4);
+
+            Assert.Throws<ArgumentException>(() => localTime.WithMinutes(64));
+            Assert.Throws<ArgumentException>(() => localTime.WithMinutes(-2));
+        }
+
+        [Fact]
+        public void Test_WithSeconds_Ok()
+        {
+            LocalTime localTime = new LocalTime(2, 4, 8).WithSeconds(4);
+            LocalTime localTimeResult = new LocalTime(2, 4, 4);
+
+            Assert.Equal(localTimeResult, localTime);
+        }
+
+        [Fact]
+        public void Test_WithSeconds_ArgumentException()
+        {
+            LocalTime localTime = new LocalTime(2, 4, 8);
+
+            Assert.Throws<ArgumentException>(() => localTime.WithSeconds(64));
+            Assert.Throws<ArgumentException>(() => localTime.WithSeconds(-2));
+        }
+
+        [Fact]
+        public void Test_WithMilliseconds_Ok()
+        {
+            LocalTime localTime = new LocalTime(2, 4, 8, 16).WithMilliseconds(512);
+            LocalTime localTimeResult = new LocalTime(2, 4, 8, 512);
+
+            Assert.Equal(localTimeResult, localTime);
+        }
+
+        [Fact]
+        public void Test_WithMilliseconds_ArgumentException()
+        {
+            LocalTime localTime = new LocalTime(2, 4, 8, 16);
+
+            Assert.Throws<ArgumentException>(() => localTime.WithSeconds(1024));
+            Assert.Throws<ArgumentException>(() => localTime.WithSeconds(-2));
+        }
+
+        [Fact]
+        public void Test_AddHours_Ok()
+        {
+            LocalTime localTime = new LocalTime(2, 4);
+            int hoursToAdd = 8;
+            LocalTime localTimeResult = new LocalTime(10, 4);
+
+            Assert.Equal(localTimeResult, localTime.AddHours(hoursToAdd));
+
+            localTime = new LocalTime(2, 4);
+            hoursToAdd = 25;
+            localTimeResult = new LocalTime(3, 4);
+
+            Assert.Equal(localTimeResult, localTime.AddHours(hoursToAdd));
+
+            localTime = new LocalTime(16, 4);
+            hoursToAdd = -4;
+            localTimeResult = new LocalTime(12, 4);
+
+            Assert.Equal(localTimeResult, localTime.AddHours(hoursToAdd));
+
+            localTime = new LocalTime(16, 4);
+            hoursToAdd = -20;
+            localTimeResult = new LocalTime(20, 4);
+
+            Assert.Equal(localTimeResult, localTime.AddHours(hoursToAdd));
+
+            localTime = new LocalTime(16, 4);
+            hoursToAdd = -26;
+            localTimeResult = new LocalTime(14, 4);
+
+            Assert.Equal(localTimeResult, localTime.AddHours(hoursToAdd));
+
+            localTime = new LocalTime(16, 4);
+            hoursToAdd = 0;
+            localTimeResult = new LocalTime(16, 4);
+
+            Assert.Equal(localTimeResult, localTime.AddHours(hoursToAdd));
+        }
+
+        [Fact]
+        public void Test_AddMinutes_Ok()
+        {
+            LocalTime localTime = new LocalTime(2, 8);
+            int minutesToAdd = 16;
+            LocalTime localTimeResult = new LocalTime(2, 24);
+
+            Assert.Equal(localTimeResult, localTime.AddMinutes(minutesToAdd));
+
+            localTime = new LocalTime(2, 8);
+            minutesToAdd = 52;
+            localTimeResult = new LocalTime(3, 0);
+
+            Assert.Equal(localTimeResult, localTime.AddMinutes(minutesToAdd));
+
+            localTime = new LocalTime(2, 8);
+            minutesToAdd = 64;
+            localTimeResult = new LocalTime(3, 12);
+
+            Assert.Equal(localTimeResult, localTime.AddMinutes(minutesToAdd));
+
+            localTime = new LocalTime(2, 8);
+            minutesToAdd = 128;
+            localTimeResult = new LocalTime(4, 16);
+
+            Assert.Equal(localTimeResult, localTime.AddMinutes(minutesToAdd));
+
+            localTime = new LocalTime(2, 8);
+            minutesToAdd = -4;
+            localTimeResult = new LocalTime(2, 4);
+
+            Assert.Equal(localTimeResult, localTime.AddMinutes(minutesToAdd));
+
+            localTime = new LocalTime(2, 8);
+            minutesToAdd = -16;
+            localTimeResult = new LocalTime(1, 52);
+
+            Assert.Equal(localTimeResult, localTime.AddMinutes(minutesToAdd));
+
+            localTime = new LocalTime(2, 8);
+            minutesToAdd = -124;
+            localTimeResult = new LocalTime(0, 4);
+
+            Assert.Equal(localTimeResult, localTime.AddMinutes(minutesToAdd));
+        }
+
+        [Fact]
+        public void Test_AddSeconds_Ok()
+        {
+            LocalTime localTime = new LocalTime(2, 4, 8);
+            int secondsToAdd = 16;
+            LocalTime localTimeResult = new LocalTime(2, 4, 24);
+
+            Assert.Equal(localTimeResult, localTime.AddSeconds(secondsToAdd));
+
+            localTime = new LocalTime(2, 4, 8);
+            secondsToAdd = 52;
+            localTimeResult = new LocalTime(2, 5, 0);
+
+            Assert.Equal(localTimeResult, localTime.AddSeconds(secondsToAdd));
+
+            localTime = new LocalTime(2, 4, 8);
+            secondsToAdd = 64;
+            localTimeResult = new LocalTime(2, 5, 12);
+
+            Assert.Equal(localTimeResult, localTime.AddSeconds(secondsToAdd));
+
+            localTime = new LocalTime(2, 4, 8);
+            secondsToAdd = 128;
+            localTimeResult = new LocalTime(2, 6, 16);
+
+            Assert.Equal(localTimeResult, localTime.AddSeconds(secondsToAdd));
+
+            localTime = new LocalTime(2, 4, 8);
+            secondsToAdd = -4;
+            localTimeResult = new LocalTime(2, 4, 4);
+
+            Assert.Equal(localTimeResult, localTime.AddSeconds(secondsToAdd));
+
+            localTime = new LocalTime(2, 4, 8);
+            secondsToAdd = -16;
+            localTimeResult = new LocalTime(2, 3, 52);
+
+            Assert.Equal(localTimeResult, localTime.AddSeconds(secondsToAdd));
+
+            localTime = new LocalTime(2, 4, 8);
+            secondsToAdd = -124;
+            localTimeResult = new LocalTime(2, 2, 4);
+
+            Assert.Equal(localTimeResult, localTime.AddSeconds(secondsToAdd));
+        }
+
+        [Fact]
+        public void Test_AddMilliseconds_Ok()
+        {
+            LocalTime localTime = new LocalTime(2, 4, 8, 16);
+            int millisecondsToAdd = 16;
+            LocalTime localTimeResult = new LocalTime(2, 4, 8, 32);
+
+            Assert.Equal(localTimeResult, localTime.AddMilliseconds(millisecondsToAdd));
+
+            localTime = new LocalTime(2, 4, 8, 16);
+            millisecondsToAdd = 984;
+            localTimeResult = new LocalTime(2, 4, 9, 0);
+
+            Assert.Equal(localTimeResult, localTime.AddMilliseconds(millisecondsToAdd));
+
+            localTime = new LocalTime(2, 4, 8, 16);
+            millisecondsToAdd = 1016;
+            localTimeResult = new LocalTime(2, 4, 9, 32);
+
+            Assert.Equal(localTimeResult, localTime.AddMilliseconds(millisecondsToAdd));
+
+            localTime = new LocalTime(2, 4, 8, 16);
+            millisecondsToAdd = 2016;
+            localTimeResult = new LocalTime(2, 4, 10, 32);
+
+            Assert.Equal(localTimeResult, localTime.AddMilliseconds(millisecondsToAdd));
+
+            localTime = new LocalTime(2, 4, 8, 16);
+            millisecondsToAdd = -8;
+            localTimeResult = new LocalTime(2, 4, 8, 8);
+
+            Assert.Equal(localTimeResult, localTime.AddMilliseconds(millisecondsToAdd));
+
+            localTime = new LocalTime(2, 4, 8, 16);
+            millisecondsToAdd = -32;
+            localTimeResult = new LocalTime(2, 4, 7, 984);
+
+            Assert.Equal(localTimeResult, localTime.AddMilliseconds(millisecondsToAdd));
+
+            localTime = new LocalTime(2, 4, 8, 16);
+            millisecondsToAdd = -2008;
+            localTimeResult = new LocalTime(2, 4, 6, 8);
+
+            Assert.Equal(localTimeResult, localTime.AddMilliseconds(millisecondsToAdd));
+        }
+
+        [Fact]
+        public void Test_ToIsoString()
+        {
+            LocalTime localTime = new LocalTime(2, 4, 8, 512);
+            string expected = "02:04:08.512";
+
+            Assert.Equal(expected, localTime.ToIsoString());
+        }
+
+        [Fact]
+        public void Test_ToString()
+        {
+            LocalTime localTime = new LocalTime(2, 4, 8, 512);
+            string expected = "02:04:08.512";
+
+            Assert.Equal(expected, localTime.ToString());
+        }
+
+        [Fact]
+        public void TestToString_WithFormat()
+        {
+            LocalTime localTime = new LocalTime(2, 4, 8, 512);
+            string expected = "02 04 08.512";
+
+            Assert.Equal(expected, localTime.ToString("HH mm ss.fff"));
+
+            localTime = new LocalTime(2, 4, 8, 512);
+            expected = "2 4 8.512";
+
+            Assert.Equal(expected, localTime.ToString("H m s.fff"));
+        }
+
+        [Fact]
+        public void TestToString_WithFormatAndFormatProvider()
+        {
+            LocalTime localTime = new LocalTime(14, 4, 8, 512);
+            string expected = "02PM 04 08.512";
+            CultureInfo cultureInfo = new CultureInfo("en-US");
+
+            Assert.Equal(expected, localTime.ToString("hhtt mm ss.fff", cultureInfo));
+        }
+
+        [Fact]
+        public void Test_Equals_True()
+        {
+            LocalTime localTime1 = new LocalTime(2, 4);
+            LocalTime localTime2 = new LocalTime(2, 4, 0, 0);
+
+            Assert.True(localTime1 == localTime2);
+            Assert.True(localTime1.Equals(localTime2));
+        }
+
+        [Fact]
+        public void Test_Equals_False()
+        {
+            LocalTime localTime1 = new LocalTime(2, 4);
+            LocalTime localTime2 = new LocalTime(2, 4, 0, 1);
+
+            Assert.False(localTime1 == localTime2);
+            Assert.False(localTime1.Equals(localTime2));
+        }
+
+        [Fact]
+        public void Test_NotEquals_True()
+        {
+            LocalTime localTime1 = new LocalTime(2, 4);
+            LocalTime localTime2 = new LocalTime(2, 4, 0, 1);
+
+            Assert.True(localTime1 != localTime2);
+        }
+
+        [Fact]
+        public void Test_NotEquals_False()
+        {
+            LocalTime localTime1 = new LocalTime(2, 4);
+            LocalTime localTime2 = new LocalTime(2, 4, 0, 0);
+
+            Assert.False(localTime1 != localTime2);
+        }
+
+        [Fact]
+        public void Test_GetHashCode_Equal()
+        {
+            LocalTime localTime1 = new LocalTime(2, 4, 8, 16);
+            LocalTime localTime2 = new LocalTime(2, 4, 8, 16);
+
+            int hashCode1 = localTime1.GetHashCode();
+            int hashCode2 = localTime2.GetHashCode();
+
+            Assert.Equal(hashCode1, hashCode2);
+        }
+
+        [Fact]
+        public void Test_GetHashCode_NotEqual()
+        {
+            LocalTime localTime1 = new LocalTime(2, 4, 8, 16);
+            LocalTime localTime2 = new LocalTime(2, 8, 4, 16);
+
+            int hashCode1 = localTime1.GetHashCode();
+            int hashCode2 = localTime2.GetHashCode();
+
+            Assert.NotEqual(hashCode1, hashCode2);
+        }
+    }
+}
---
+++ NuniToolbox/Time/LocalTime.cs
@@ -0,0 +1,363 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NuniToolbox.Time
+{
+    /// <summary>
+    /// Represents a time without any time zone information as an immutable struct.
+    /// Methods of <see cref="LocalTime" /> may depend on <see cref="DateTime" /> to reuse its logic.
+    /// Therefore <see cref="LocalTime" /> can be seen as some kind of wrapper around <see cref="DateTime" /> providing only local time relevant APIs.
+    /// </summary>
+    public struct LocalTime
+    {
+        private readonly int m_hours;
+        private readonly int m_minutes;
+        private readonly int m_seconds;
+        private readonly int m_milliseconds;
+
+        /// <summary>
+        /// Returns the hours component.
+        /// </summary>
+        public int Hours
+        {
+            get
+            {
+                return m_hours;
+            }
+        }
+
+        /// <summary>
+        /// Returns the minutes component.
+        /// </summary>
+        public int Minutes
+        {
+            get
+            {
+                return m_minutes;
+            }
+        }
+
+        /// <summary>
+        /// Returns the milliseconds component.
+        /// </summary>
+        public int Milliseconds
+        {
+            get
+            {
+                return m_milliseconds;
+            }
+        }
+
+        /// <summary>
+        /// Returns the seconds component.
+        /// </summary>
+        public int Seconds
+        {
+            get
+            {
+                return m_seconds;
+            }
+        }
+
+        /// <summary>
+        /// Create a new <see cref="LocalTime" />.
+        /// </summary>
+        /// <param name="hours"></param>
+        /// <param name="minutes"></param>
+        public LocalTime(int hours, int minutes) : this(hours, minutes, 0) { }
+
+        /// <summary>
+        /// Create a new <see cref="LocalTime" />.
+        /// </summary>
+        /// <param name="hours"></param>
+        /// <param name="minutes"></param>
+        /// <param name="seconds"></param>
+        public LocalTime(int hours, int minutes, int seconds) : this(hours, minutes, seconds, 0) { }
+
+        /// <summary>
+        /// Create a new <see cref="LocalTime" />.
+        /// </summary>
+        /// <param name="hours"></param>
+        /// <param name="minutes"></param>
+        /// <param name="seconds"></param>
+        /// <param name="milliseconds"></param>
+        public LocalTime(int hours, int minutes, int seconds, int milliseconds)
+        {
+            CheckHours(hours);
+            CheckMinutes(minutes);
+            CheckSeconds(seconds);
+            CheckMilliseconds(milliseconds);
+
+            m_hours = hours;
+            m_minutes = minutes;
+            m_seconds = seconds;
+            m_milliseconds = milliseconds;
+        }
+
+        /// <summary>
+        /// Create a new <see cref="LocalTime" /> out of the time components of the <see cref="DateTime" />.
+        /// </summary>
+        /// <param name="dateTime"></param>
+        public LocalTime(DateTime dateTime)
+        {
+            m_hours = dateTime.Hour;
+            m_minutes = dateTime.Minute;
+            m_seconds = dateTime.Second;
+            m_milliseconds = dateTime.Millisecond;
+        }
+
+        /// <summary>
+        /// Converts this <see cref="LocalTime" /> into a <see cref="DateTime" />.
+        /// </summary>
+        /// <returns></returns>
+        public DateTime ToDateTime()
+        {
+            return DateTime.Today
+                .AddHours(m_hours)
+                .AddMinutes(m_minutes)
+                .AddSeconds(m_seconds)
+                .AddMilliseconds(m_milliseconds);
+        }
+
+        private static void CheckHours(int hours)
+        {
+            if (hours < 0 || hours > 23)
+            {
+                throw new ArgumentException($"{hours} must be between 0 and 23");
+            }
+        }
+
+        private static void CheckMinutes(int minutes)
+        {
+            if (minutes < 0 || minutes > 59)
+            {
+                throw new ArgumentException($"{minutes} must be between 0 and 59");
+            }
+        }
+
+        private static void CheckSeconds(int seconds)
+        {
+            if (seconds < 0 || seconds > 59)
+            {
+                throw new ArgumentException($"{seconds} must be between 0 and 59");
+            }
+        }
+
+        private static void CheckMilliseconds(int milliseconds)
+        {
+            if (milliseconds < 0 || milliseconds > 999)
+            {
+                throw new ArgumentException($"{milliseconds} must be between 0 and 999");
+            }
+        }
+
+        /// <summary>
+        /// Returns a new <see cref="LocalTime" /> with the specified hours.
+        /// </summary>
+        /// <param name="hours"></param>
+        /// <returns></returns>
+        public LocalTime WithHours(int hours)
+        {
+            CheckHours(hours);
+
+            return new LocalTime(hours, m_minutes, m_seconds, m_milliseconds);
+        }
+
+        /// <summary>
+        /// Returns a new <see cref="LocalTime" /> with the specified minutes.
+        /// </summary>
+        /// <param name="minutes"></param>
+        /// <returns></returns>
+        public LocalTime WithMinutes(int minutes)
+        {
+            CheckMinutes(minutes);
+
+            return new LocalTime(m_hours, minutes, m_seconds, m_milliseconds);
+        }
+
+        /// <summary>
+        /// Returns a new <see cref="LocalTime" /> with the specified seconds.
+        /// </summary>
+        /// <param name="seconds"></param>
+        /// <returns></returns>
+        public LocalTime WithSeconds(int seconds)
+        {
+            CheckSeconds(seconds);
+
+            return new LocalTime(m_hours, m_minutes, seconds, m_milliseconds);
+        }
+
+        /// <summary>
+        /// Returns a new <see cref="LocalTime" /> with the specified milliseconds.
+        /// </summary>
+        /// <param name="milliseconds"></param>
+        /// <returns></returns>
+        public LocalTime WithMilliseconds(int milliseconds)
+        {
+            CheckMilliseconds(milliseconds);
+
+            return new LocalTime(m_hours, m_minutes, m_seconds, milliseconds);
+        }
+
+        /// <summary>
+        /// Returns a new <see cref="LocalTime" /> by adding the specified amount of hours.
+        /// </summary>
+        /// <param name="hours"></param>
+        /// <returns></returns>
+        public LocalTime AddHours(int hours)
+        {
+            /*if (hours == 0)
+            {
+                return new LocalTime(m_hours, m_minutes, m_seconds, m_milliseconds);
+            }
+
+            int newHours = Math.Abs(m_hours + hours) % 24;
+
+            if (hours < 0)
+            {
+                newHours = 24 - newHours;
+            }
+
+            return new LocalTime(newHours, m_minutes, m_seconds, m_milliseconds);*/
+
+            if (hours == 0)
+            {
+                return new LocalTime(m_hours, m_minutes, m_seconds, m_milliseconds);
+            }
+
+            return new LocalTime(ToDateTime().AddHours(hours));
+        }
+
+        /// <summary>
+        /// Returns a new <see cref="LocalTime" /> by adding the specified amount of minutes.
+        /// </summary>
+        /// <param name="minutes"></param>
+        /// <returns></returns>
+        public LocalTime AddMinutes(int minutes)
+        {
+            if (minutes == 0)
+            {
+                return new LocalTime(m_hours, m_minutes, m_seconds, m_milliseconds);
+            }
+
+            return new LocalTime(ToDateTime().AddMinutes(minutes));
+        }
+
+        /// <summary>
+        /// Returns a new <see cref="LocalTime" /> by adding the specified amount of seconds.
+        /// </summary>
+        /// <param name="seconds"></param>
+        /// <returns></returns>
+        public LocalTime AddSeconds(int seconds)
+        {
+            if (seconds == 0)
+            {
+                return new LocalTime(m_hours, m_minutes, m_seconds, m_milliseconds);
+            }
+
+            return new LocalTime(ToDateTime().AddSeconds(seconds));
+        }
+
+        /// <summary>
+        /// Returns a new <see cref="LocalTime" /> by adding the specified amount of milliseconds.
+        /// </summary>
+        /// <param name="milliseconds"></param>
+        /// <returns></returns>
+        public LocalTime AddMilliseconds(int milliseconds)
+        {
+            if (milliseconds == 0)
+            {
+                return new LocalTime(m_hours, m_minutes, m_seconds, m_milliseconds);
+            }
+
+            return new LocalTime(ToDateTime().AddMilliseconds(milliseconds));
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj != null && obj is LocalTime otherLocalTime)
+            {
+                return this == otherLocalTime;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 1365636279;
+            hashCode = hashCode * -1521134295 + m_hours.GetHashCode();
+            hashCode = hashCode * -1521134295 + m_minutes.GetHashCode();
+            hashCode = hashCode * -1521134295 + m_seconds.GetHashCode();
+            hashCode = hashCode * -1521134295 + m_milliseconds.GetHashCode();
+
+            return hashCode;
+        }
+
+        /// <summary>
+        /// Formats this <see cref="LocalTime"/> as ISO 8601 compliant time string (HH:mm:ss.fff -> 02:04:08.512).
+        /// </summary>
+        /// <returns></returns>
+        public string ToIsoString()
+        {
+            return string.Format("{0}:{1}:{2}.{3}", m_hours.ToString("00"), m_minutes.ToString("00"), m_seconds.ToString("00"), m_milliseconds.ToString("000"));
+        }
+
+        /// <summary>
+        /// Formats this <see cref="LocalTime"/> as ISO 8601 compliant time string (HH:mm:ss.fff -> 02:04:08.512).
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return ToIsoString();
+        }
+
+        /// <summary>
+        /// Formats this <see cref="LocalTime"/> using the specified format.
+        /// </summary>
+        /// <param name="format"></param>
+        /// <returns></returns>
+        public string ToString(string format)
+        {
+            return ToDateTime().ToString(format);
+        }
+
+        /// <summary>
+        /// Formats this <see cref="LocalTime"/> using the specified format provider.
+        /// </summary>
+        /// <param name="formatProvider"></param>
+        /// <returns></returns>
+        public string ToString(IFormatProvider formatProvider)
+        {
+            return ToDateTime().ToString(formatProvider);
+        }
+
+
+        /// <summary>
+        /// Formats this <see cref="LocalTime"/> using the specified format and format provider.
+        /// </summary>
+        /// <param name="format"></param>
+        /// <param name="formatProvider"></param>
+        /// <returns></returns>
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            return ToDateTime().ToString(format, formatProvider);
+        }
+
+        public static bool operator ==(LocalTime localTime, LocalTime otherLocalTime)
+        {
+            return localTime.m_hours == otherLocalTime.m_hours
+                && localTime.m_minutes == otherLocalTime.m_minutes
+                && localTime.m_seconds == otherLocalTime.m_seconds
+                && localTime.m_milliseconds == otherLocalTime.m_milliseconds;
+        }
+
+        public static bool operator !=(LocalTime localTime, LocalTime otherLocalTime)
+        {
+            return !(localTime == otherLocalTime);
+        }
+    }
+}
