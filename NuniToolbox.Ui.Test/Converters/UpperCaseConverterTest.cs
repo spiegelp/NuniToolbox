97ce185ca07fae619761bc109e87154f2cf0bf95
Philipp Spiegel <spiegelp@gmx.at>
2020-01-18 12:35:46 +0100

    added converter for converting strings to upper case

---
+++ NuniToolbox.Ui.Test/Converters/UpperCaseConverterTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+using Xunit;
+
+using NuniToolbox.Ui.Converters;
+
+namespace NuniToolbox.Ui.Test.Converters
+{
+    public class UpperCaseConverterTest
+    {
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("  ", "  ")]
+        [InlineData(".NET Core", ".NET CORE")]
+        [InlineData("Schließen", "SCHLIESSEN")]
+        [InlineData(2, null)]
+        [InlineData(null, null)]
+        public void Test_Convert_Ok(object str, string expected)
+        {
+            UpperCaseConverter converter = new UpperCaseConverter();
+
+            Assert.Equal(expected, converter.Convert(str, typeof(string), null, CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ NuniToolbox.Ui/Converters/UpperCaseConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace NuniToolbox.Ui.Converters
+{
+    /// <summary>
+    /// Converts a string to upper case.
+    /// </summary>
+    public class UpperCaseConverter : IValueConverter
+    {
+        /// <summary>
+        /// Creates a new <see cref="UpperCaseConverter" />.
+        /// </summary>
+        public UpperCaseConverter() { }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null && value is string str)
+            {
+                return str.ToUpper(culture).Replace("ß", "SS");
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
