c9b837645306c5b7c1297ddff257c9a3e985477b
Philipp Spiegel <spiegelp@gmx.at>
2020-03-06 22:22:11 +0100

    added extension methods for shallow and deep copy

---
+++ NuniToolbox.Test/Objects/ObjectExtensionsTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xunit;
+
+using NuniToolbox.Objects;
+
+namespace NuniToolbox.Test.Objects
+{
+    public class ObjectExtensionsTest
+    {
+        [Fact]
+        public void Test_ShallowCopy_Ok()
+        {
+            Person person = new Person { Name = "Boy", Age = 32, HasPets = true, Mother = new Person { Name = "Boy's mother", Age = 64, HasPets = null } };
+            Person clonedPerson = person.ShallowCopy();
+
+            Assert.False(person == clonedPerson);
+            Assert.Equal(person.Name, clonedPerson.Name);
+            Assert.Equal(person.Age, clonedPerson.Age);
+            Assert.Equal(person.HasPets, clonedPerson.HasPets);
+            Assert.True(person.Mother == clonedPerson.Mother);
+        }
+
+        [Fact]
+        public void Test_DeepCopy_Ok()
+        {
+            Person person = new Person { Name = "Boy", Age = 32, HasPets = true, Mother = new Person { Name = "Boy's mother", Age = 64, HasPets = null } };
+            Person clonedPerson = person.DeepCopy();
+
+            Assert.False(person == clonedPerson);
+            Assert.Equal(person.Name, clonedPerson.Name);
+            Assert.Equal(person.Age, clonedPerson.Age);
+            Assert.Equal(person.HasPets, clonedPerson.HasPets);
+            Assert.True(person.Mother != clonedPerson.Mother);
+            Assert.Equal(person.Mother.Name, clonedPerson.Mother.Name);
+            Assert.Equal(person.Mother.Age, clonedPerson.Mother.Age);
+            Assert.Equal(person.Mother.HasPets, clonedPerson.Mother.HasPets);
+        }
+
+        public class Person
+        {
+            public string Name { get; set; }
+
+            public int Age { get; set; }
+
+            public Person Mother { get; set; }
+
+            public bool? HasPets { get; set; }
+
+            public Person()
+            {
+                Name = null;
+                Age = 0;
+                HasPets = null;
+            }
+        }
+    }
+}
---
+++ NuniToolbox/Objects/ObjectExtensions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+using NuniToolbox.Collections;
+
+namespace NuniToolbox.Objects
+{
+    /// <summary>
+    /// A class with generic extension methods for objects.
+    /// </summary>
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Returns a shallow copy of the speficied object.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static T ShallowCopy<T>(this T obj) where T : new()
+        {
+            T clonedObj = new T();
+
+            clonedObj.GetType().GetProperties()
+                .Where(propertyInfo => propertyInfo.CanRead && propertyInfo.CanWrite)
+                .Foreach(propertyInfo => propertyInfo.SetValue(clonedObj, propertyInfo.GetValue(obj)));
+
+            return clonedObj;
+        }
+
+        /// <summary>
+        /// Returns a deep copy of the speficied object.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static T DeepCopy<T>(this T obj) where T : new()
+        {
+            return (T)DeepCopy(obj, obj.GetType());
+        }
+
+        private static object DeepCopy(object obj, Type type)
+        {
+            if (obj != null)
+            {
+                object clonedObj = type.GetConstructor(Type.EmptyTypes).Invoke(Type.EmptyTypes);
+
+                type.GetProperties()
+                    .Where(propertyInfo => propertyInfo.CanRead && propertyInfo.CanWrite)
+                    .Foreach(propertyInfo =>
+                    {
+                        if (propertyInfo.PropertyType.IsValueType || propertyInfo.PropertyType == typeof(string))
+                        {
+                            propertyInfo.SetValue(clonedObj, propertyInfo.GetValue(obj));
+                        }
+                        else
+                        {
+                            object clonedProperty = DeepCopy(propertyInfo.GetValue(obj), propertyInfo.PropertyType);
+                            propertyInfo.SetValue(clonedObj, clonedProperty);
+                        }
+                    });
+
+                return clonedObj;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
